318973b67b1fcf19d61754440359cf5c9ee19cf9
Vyacheslav Lopachev <vmlopach@gmail.com>
2018-11-23 08:22:16 +0600

    add logger

--- src/main/java/com/epam/parser/Runner.java
+++ src/main/java/com/epam/parser/Runner.java
@@ -4,10 +4,14 @@ import com.epam.parser.entity.Paragraph;
 import com.epam.parser.entity.Text;
 import com.epam.parser.logic.PropertyManager;
 import com.epam.parser.logic.Util;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 
 import java.util.List;
 
 public class Runner {
+    private static Logger logger = LogManager.getLogger(Runner.class);
     public static void main(String[] args) {
         Text text = new Text();
         text.setText(Util.getTextFromFile("text1.txt"));
@@ -15,9 +19,10 @@ public class Runner {
         text.setPropertyManager(propertyManager);
         text.parse();
         List<Paragraph> paragraphs = text.getParagraphs();
-        for (Paragraph paragraph: paragraphs){
-            System.out.println("Paragraph1:");
-            System.out.println(paragraph.getText());
-        }
+//        for (Paragraph paragraph: paragraphs){
+//            System.out.println("Paragraph1:");
+//            System.out.println(paragraph.getText());
+//        }
+        logger.info("это логгер");
     }
 }
